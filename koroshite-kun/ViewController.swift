11c9b7f8dfda312d5e96f7aaaa8e1ec63060bf92
minako-ph <chanmina143@gmail.com>
2020-08-27 00:55:56 +0900

    Initial Commit

---
+++ koroshite-kun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  koroshite-kun
+//
+//  Created by Minako Redrendo Yamamoto on 2020/08/27.
+//  Copyright © 2020 Minako Redrendo Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
